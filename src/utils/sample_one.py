acd21750bbad41cbea1c16f7e6995f602524e652
Alwaz18 <alwazk8@gmail.com>
2022-02-24 02:29:27 +0500

    init

--- src/utils/sample_one.py
+++ src/utils/sample_one.py
@@ -1,3 +1,7 @@
+import os
+import json
+from flask import render_template
+
 def showjson():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static/data", "taiwan.json")
